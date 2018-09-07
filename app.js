40a7ab9f2a818add602a289922e9e5f0d082a618
corner82 <311corner82@gmail.com>
2018-09-07 08:20:53 +0300

    offline try-catch bloğu eklendi, log4js kütüphaneleri eklendi
    offline try-catch bloğu eklendi, log4js kütüphaneleri eklendi

--- app.js
+++ app.js
@@ -5,6 +5,50 @@ var https = require('https');
 //var io = require('socket.io');
 
 
+log4js = require("log4js");
+//log4js.configure(__dirname + "/log4js.json") 
+
+log4js.configure({
+  appenders: {
+    email: {
+      type: '@log4js-node/smtp',
+	  //type: 'smtp',
+      transport: {
+        plugin: 'smtp',
+        options: {
+          host: 'smtp.gmail.com',
+          port: 587
+        }
+      },
+      recipients: '311corner82@gmail.com'
+    }
+  },
+  categories: {
+    default: { appenders: ['email'], level: 'info' }
+  }
+});
+													 
+/*log4js.configure({
+  appenders: {
+    'email': {
+      type: '@log4js-node/smtp',
+      recipients: '311corner82@gmail.com',
+      subject: 'Latest logs',
+      sender: '311corner82@gmail.com',
+      attachment: {
+        enable: true,
+        //filename: 'latest.log',
+        message: 'See the attachment for the latest logs'
+      },
+      sendInterval: 3600
+    }
+  },
+  categories: { default: { appenders: ['email'], level: 'ERROR' } }
+});	*/											
+													 
+logger = log4js.getLogger();
+
+
 
 /*pem.createCertificate({days:1, selfSigned:true}, function(err, keys){
   var app = express();
@@ -51,7 +95,15 @@ server.listen(3000);
 //});
 
 io.sockets.on('connection', function(socket) {
-		console.log('io socket connceted');
+		console.log('io socket connted');
+		
+		socket.on('error', (err) => {
+		  // If the connection is reset by the server, or if it can't
+		  // connect at all, or on any sort of error encountered by
+		  // the connection, the error will be sent here.
+		  console.error(err);
+		});
+		
         socket.on('set user', function(data){
             if(nickNames.indexOf(data.userName)!=-1) {
                 console.log(data.userName+' already added');
@@ -69,19 +121,34 @@ io.sockets.on('connection', function(socket) {
 		//socket.broadcast.emit('new message', data);
 		
 		
-		// rabbitMQ publish
+	// rabbitMQ publish
 	var amqp = require('amqplib/callback_api');
 
 	amqp.connect('amqp://localhost', function(err, conn) {
-	  conn.createChannel(function(err, ch) {
-		var q = 'pageLogRabbit';
-		  var msg = '{ "UserName" : "test","Host" : "localhost:3000","Action": "test action","Controller":"Controller","Port":"3000","UserAgent":"Chrome","UserIP":"127.0.0.1","Method":"Demo","SessionID": "test session id","UserToken":"ssssss","UserPublicKey":"eeeeee","UserPrivateKey":"asaass"}';
+		
+		try{ 
+			logger.error("This send you an email!");
+			conn.createChannel(function(err, ch) {
+			//var q = 'pageLogRabbit';
+			var q = 'pageActivityLogRabbit';
+			
+			  var msg = '{ "UserName" : "test","Host" : "localhost:3000","Action": "test action","Controller":"Controller","Port":"3000","UserAgent":"Chrome","UserIP":"127.0.0.1","Method":"Demo","SessionID": "test session id","UserToken":"ssssss","UserPublicKey":"eeeeee","UserPrivateKey":"asaass"}';
 
-			ch.assertQueue(q, {durable: false});
-			ch.sendToQueue(q, Buffer.from(msg));
-			console.log(" [x] Sent %s", msg);
-		  });
+				ch.assertQueue(q, {durable: false});
+				//ch.sendToQueue(q, Buffer.from(msg));
+				ch.sendToQueue(q, Buffer.from(data));
+				//console.log(" [x] Sent %s", msg);
+				console.log(" [x] Sent %s", data);
+			  });
+			
+		} catch(err) {
+			logger.error("This send you an email!");
+			console.log(err);
+		}
+		
+		  
 		  //setTimeout(function() { conn.close(); process.exit(0) }, 500);
+		  
 		});
 		
 		
